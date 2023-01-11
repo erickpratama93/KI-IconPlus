b6449f5cca1b7526e0a8c5e9251dbeee58e4cb8f
ardyassetya <ardyassetya@gmail.com>
2023-01-11 11:17:09 +0700

    asasas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,9 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<<<<<<< HEAD
 
-=======
->>>>>>> 91c34c7d3c747004fcdc4d5c894a4dfcb4e9fb3d
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -20,10 +17,6 @@
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
-<<<<<<< HEAD
-
-=======
->>>>>>> 91c34c7d3c747004fcdc4d5c894a4dfcb4e9fb3d
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
@@ -45,11 +38,6 @@
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
                         @guest
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
->>>>>>> parent of ed65072 (nama user)
                         @if (Route::has('login'))
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
@@ -61,65 +49,7 @@
                             <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                         </li>
                         @endif
-                        @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-<<<<<<< HEAD
-                                     document.getElementById('logout-form').submit();">
-=======
-                                                     document.getElementById('logout-form').submit();">
->>>>>>> parent of ed65072 (nama user)
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-<<<<<<< HEAD
-
-
-</html>
-
-</html>
-=======
-=======
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
->>>>>>> 91c34c7d3c747004fcdc4d5c894a4dfcb4e9fb3d
+                        
                         @endguest
                     </ul>
                 </div>
@@ -131,10 +61,7 @@
         </main>
     </div>
 </body>
-<<<<<<< HEAD
+
 
 </html>
-=======
-</html>
->>>>>>> 91c34c7d3c747004fcdc4d5c894a4dfcb4e9fb3d
->>>>>>> parent of ed65072 (nama user)
+
